ffcf6bd4f4ea3ada9bc708d222870492a87db71a
payal <payalm.goldenmace@gmail.com>
2025-06-13 12:23:57 +0530

    remove title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PayPal Demo payal',
+      title: 'PayPal Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: PaymentScreen(),
     );
